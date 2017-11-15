448d46adf672c539553c3154c6e385f2d5526aaf
HiddenD <33227612+HiddenD@users.noreply.github.com>
2017-11-15 11:49:20 -0500

    Add files via upload
    Distance Traveled Homework

---
+++ M4HW1_DistanceTraveled_ReginaldJones.py
@@ -0,0 +1,19 @@
+#CTI-110
+#M4HW1 - Distance Traveled
+#Reginald Jones
+#Novemeber 15, 2017
+#
+
+#Formula for Distance
+#distance = speed * time
+
+#Calculating the Distance Traveled
+speed = 70
+distanceAfter6 = speed * 6
+distanceAfter10 = speed * 10
+distanceAfter15 = speed * 15
+
+#Displaying the Distance Traveled
+print('The Distance Traveled After 6 Hours is',(distanceAfter6))
+print('The Distance Traveled After 10 Hours is',(distanceAfter10))
+print('The Distance Traveled After 15 Hours is',(distanceAfter15))
